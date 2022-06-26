2894541d8598d403aed9f4e3efe1d16738f7aaf9
Kei <opusk44@gmail.com>
2022-06-26 16:40:35 +0900

    change quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,19 +1,44 @@
 const quotes = [
   {
-    quote: "I'm not a great programmer; I'm just a good programmer with great habits.",
-    author: "Kent Beck"
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
   },
   {
-    quote: "First, solve the problem. Then, write the code.",
-    author: "John Johnson"
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
   },
   {
-    quote: "It's not a bug - it's an undocumented feature.",
-    author: "Developer"
+    quote:
+      "The world is a book and those who do not travel read only one page.",
+    author: "Saint Augustine",
   },
   {
-    quote: "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
-    author: "Martin Golding"
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+  },
+  {
+    quote: "To Travel is to Live",
+    author: "Hans Christian Andersen",
+  },
+  {
+    quote: "Only a life lived for others is a life worthwhile.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+  },
+  {
+    quote: "Never go on trips with anyone you do not love.",
+    author: "Hemmingway",
+  },
+  {
+    quote: "We wander for distraction, but we travel for fulfilment.",
+    author: "Hilaire Belloc",
+  },
+  {
+    quote: "Travel expands the mind and fills the gap.",
+    author: "Sheda Savage",
   },
 ];
 
